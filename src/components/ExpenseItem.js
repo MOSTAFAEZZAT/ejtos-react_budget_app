5882b8abc97b546846d5365fe2026d2cca97e845
MOSTAFAEZZAT <mostafaezzat318@gmail.com>
2023-11-15 10:23:00 +0200

    Update the repo

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,1 +1,53 @@
+import React, { useContext } from 'react';
+import { AiFillPlusCircle, AiFillMinusCircle } from 'react-icons/ai';
+import { TiDelete } from 'react-icons/ti'
+import { AppContext } from '../context/AppContext';
 
+const ExpenseItem = (props) => {
+    const { dispatch, currency } = useContext(AppContext);
+
+    const handleDeleteExpense = () => {
+        dispatch({
+            type: 'DELETE_EXPENSE',
+            payload: props.id,
+        });
+    };
+
+    const increaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        });
+
+    }
+
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+
+    }
+
+    return (
+        <tr>
+            <td>{props.name}</td>
+            <td>{currency}{props.cost}</td>
+            <td><AiFillPlusCircle color='green' size='2.5em' onClick={event => increaseAllocation(props.name)}>+</AiFillPlusCircle></td>
+            <td><AiFillMinusCircle color='red' size='2.5em' onClick={event => decreaseAllocation(props.name)}>-</AiFillMinusCircle ></td>
+            <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+        </tr>
+    );
+};
+
+export default ExpenseItem;
